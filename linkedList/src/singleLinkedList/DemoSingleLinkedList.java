1288349c9a3cc5574ce538db20775a6047453470
thejavamonk <thejavamonk@gmail.com>
2017-03-21 23:29:00 +0530

    example implementation of singlelinked list

--- linkedList/src/singleLinkedList/DemoSingleLinkedList.java
+++ linkedList/src/singleLinkedList/DemoSingleLinkedList.java
@@ -4,21 +4,41 @@ import java.util.Scanner;
 
 public class DemoSingleLinkedList {
 
+	private static Scanner scanner;
+
 	public static void main(String[] args) {
 		
-		int choice, data, k, x;
+		int choice, data;
 		
-		Scanner scanner = new Scanner(System.in);
+		scanner = new Scanner(System.in);
 		SingleLinkedList linkedList = new SingleLinkedList();
 		linkedList.createList();
 		
 		while(true){
 			System.out.println("1. Display List");
+			System.out.println("2. Count the number of nodes");
+			System.out.println("3. Search for an element");
+			System.out.println("4. Insert in empty list/Insert at the begining of the list");
+			System.out.println("5. Insert a node at the end of the list");
+			System.out.println("6. Insert a node after a specified node");
+			System.out.println("7. Insert a node befor a specified node");
+			System.out.println("8. Insert a node at a given position");
+			System.out.println("9. Delete first node");
+			System.out.println("10. Delete last node");
+			System.out.println("11. Delete any node");
+			System.out.println("12. Reverse the list");
+			System.out.println("13. Bubble sort by exchanging data");
+			System.out.println("14. Bubble sort by exchanging links");
+			System.out.println("15. Merge sort");
+			System.out.println("16. Insert Cycle");
+			System.out.println("17. Detect Cycle");
+			System.out.println("18. Remove Cycle");
+			System.out.println("19. Quit");
 			
 			System.out.println("Enter your choice :");
 			choice = scanner.nextInt();
 			
-			if(choice == 2){
+			if(choice == 19){
 				break;
 			}
 			
@@ -27,6 +47,20 @@ public class DemoSingleLinkedList {
 			case 1:
 				linkedList.displayList();
 				break;
+			case 2:
+				linkedList.countNodes();
+			case 3:
+				System.out.println("Enter the element to be searched : ");
+				data = scanner.nextInt();
+				linkedList.search(data);
+			case 4:
+				System.out.println("Enter the element to be inserted : ");
+				data = scanner.nextInt();
+				linkedList.insertInBegining(data);
+			case 5:
+				System.out.println("Enter the element to be inserted");
+				data = scanner.nextInt();
+				linkedList.insertAtEnd(data);
 			default:
 				System.out.println("No match found for the choice");
 				break;
--- linkedList/src/singleLinkedList/SingleLinkedList.java
+++ linkedList/src/singleLinkedList/SingleLinkedList.java
@@ -1,8 +1,11 @@
 package singleLinkedList;
 
+import java.util.Scanner;
+
 public class SingleLinkedList {
 
 	private Node start;
+	private Scanner scanner;
 
 	public SingleLinkedList() {
 		start = null;
@@ -30,36 +33,73 @@ public class SingleLinkedList {
 		Node p = start;
 
 		while (p != null) {
-			n ++;
+			n++;
 			p = p.link;
 		}
-		System.out.println("Number of nodes in the list = "+n);
+		System.out.println("Number of nodes in the list = " + n);
 	}
-	
-	public boolean search(int x){
-		
+
+	public boolean search(int x) {
+
 		Node p;
-		
+
 		int position = 1;
 		p = start;
-		
-		while(p != null){
-			if(p.info == x){
+
+		while (p != null) {
+			if (p.info == x) {
 				break;
 			}
 			position++;
 			p = p.link;
 		}
-		if(p == null){
-			System.out.println(x +" Not found in the list");
+		if (p == null) {
+			System.out.println(x + " Not found in the list");
 			return false;
-		}else{
-			System.out.println(x + " Is at position "+ position);
+		} else {
+			System.out.println(x + " Is at position " + position);
 			return true;
 		}
 	}
 
 	public void createList() {
 		
+		int i, n, data;
+		scanner = new Scanner(System.in);
+		System.out.println("Enter the number of nodes : ");
+		n = scanner.nextInt();
+		
+		if(n == 0){
+			return;
+		}
+		
+		for (i = 1; i <= n; i++){
+			System.out.println("Enter the element to be inserted : ");
+			data = scanner.nextInt();
+			insertAtEnd(data);
+		}
+	}
+	
+	public void insertInBegining(int data){
+		
+		Node temp = new Node(data);
+		temp.link = start;
+		start = temp;
+	}
+	
+	public void insertAtEnd(int data){
+		
+		Node p;
+		Node temp = new Node (data);
+		
+		if(start == null){
+			start = temp;
+			return;
+		}
+		p = start;
+		while (p.link != null){
+			p = p.link;
+		}
+		p.link = temp;
 	}
 }
