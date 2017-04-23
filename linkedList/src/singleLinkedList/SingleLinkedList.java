940d6664d4ce04f6011f9d6cd7ce9c4f268418c8
thejavamonk <thejavamonk@gmail.com>
2017-04-23 20:24:26 +0530

    added methods to perform insertion, deletion

--- linkedList/src/singleLinkedList/SingleLinkedList.java
+++ linkedList/src/singleLinkedList/SingleLinkedList.java
@@ -102,4 +102,145 @@ public class SingleLinkedList {
 		}
 		p.link = temp;
 	}
+	
+	public void insertAfter(int data, int x){
+		
+		Node p = start;
+		while(p != null){
+			
+			if(p.info == x){
+				break;
+			}
+			p = p.link;
+		}
+		
+		if(p == null){
+			System.out.println(x + "not present in the list");
+		}else{
+			Node temp = new Node(data);
+			temp.link = p.link;
+			p.link = temp;
+		}
+	}
+	
+	public void insertBefore(int data, int x){
+		
+		Node temp;
+		
+		if(start == null){
+			System.out.println("List is empty");
+			return;
+		}
+		
+		if(x == start.info){
+			
+			temp = new Node(data);
+			temp.link = start;
+			start = temp;
+			return;
+		}
+		
+		Node p = start;
+		while(p.link != null){
+			
+			if(p.link.info == x){
+				break;
+			}
+			p = p.link;
+		}
+		
+		if(p.link == null){
+			System.out.println(x +" not present in the list");
+		}else{
+			temp = new Node(data);
+			temp.link = p.link;
+			p.link = temp;
+		}
+	}
+	
+	public void insertAtPosition(int data, int k){
+		
+		Node temp;
+		int i;
+		
+		if(k == 1){
+			temp = new Node(data);
+			temp.link = start;
+			start = temp;
+			return;
+		}
+		
+		Node p = start;
+		for(i = 1; i < k-1 && p != null; i++){
+			p = p.link;
+		}
+		
+		if(p == null){
+			System.out.println("You can insert only up to "+ i +" th position");
+		}else{
+			
+			temp = new Node(data);
+			temp.link = p.link;
+			p.link = temp;
+		}
+	}
+	
+	public void deleteFirstNode(){
+		
+		if(start == null){
+			return;
+		}
+		start = start.link;
+	}
+	
+	public void deleteLastNode(){
+		
+		if(start == null)
+			return;
+		
+		if(start.link == null){
+			start = null;
+			return;
+		}
+		
+		Node p = start;
+		while(p.link.link != null){
+			
+			p = p.link;
+			p.link = null;
+		}
+	}
+	
+	public void deleteNode(int x){
+		
+		if(start == null){
+			
+			System.out.println("List is empty\n");
+			return;
+		}
+		
+		if(start.info == x){
+			start = start.link;
+			return;
+		}
+		
+		Node p = start;
+		while(p.link != null){
+			
+			if(p.link.info == x){
+				break;
+			}
+			p = p.link;
+		}
+		
+		if(p.link == null){
+			
+			System.out.println("Element "+ x +" is not in the list");
+		}else{
+			
+			p.link = p.link.link;
+		}
+	}
+	
+	
 }
