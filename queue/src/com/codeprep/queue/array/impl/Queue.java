31c66072e66687ba5738f065f9967c2b92b39ed9
thejavamonk <thejavamonk@gmail.com>
2018-02-11 21:08:21 +0530

    array implementation of a queue

---
+++ queue/src/com/codeprep/queue/array/impl/Queue.java
@@ -0,0 +1,91 @@
+package com.codeprep.queue.array.impl;
+
+import java.util.NoSuchElementException;
+
+public class Queue {
+	
+	private int [] queueArray;
+	private int front;
+	private int rear;
+	
+	public Queue(){
+		
+		queueArray = new int[10];
+		front = -1;
+		rear = -1;
+	}
+	
+	public Queue(int maxSize){
+		
+		queueArray = new int[maxSize];
+		front = -1;
+		rear = -1;
+	}
+	
+	public boolean isEmpty(){
+		
+		return (front == -1 || front == rear + 1);
+	}
+	
+	public boolean isFull(){
+		
+		return (rear == queueArray.length -1);
+	}
+	
+	public int size(){
+		
+		if(isEmpty()){
+			return 0;
+		}else{
+			return rear - front + 1;
+		}
+	}
+	
+	public void insert(int x){
+		
+		if(isFull()){
+			System.out.println("Queue Overflow");
+			return;
+		}
+		if(front == -1){
+			front++;
+		}
+		rear++;
+		queueArray[rear] = x;
+	}
+	
+	public int delete(){
+		
+		int x;
+		if(isEmpty()){
+			System.out.println("Queue Underflow");
+			throw new NoSuchElementException();
+		}
+		x = queueArray[front];
+		front = front + 1;
+		return x;
+	}
+	
+	public int peek(){
+		
+		if(isEmpty()){
+			System.out.println("Queue Underflow");
+			throw new NoSuchElementException();
+		}
+		return queueArray[front];
+	}
+	
+	public void display(){
+		
+		int i;
+		if(isEmpty()){
+			System.out.println("Queue is empty");
+			return;
+		}
+		System.out.println("Queue is :");
+		for(i = front; i <= rear; i++){
+			System.out.print(queueArray[i]+" ");
+		}
+		System.out.println();
+	}
+}
---
+++ queue/src/com/codeprep/queue/array/impl/QueueUsingArrayDemo.java
@@ -0,0 +1,10 @@
+package com.codeprep.queue.array.impl;
+
+public class QueueUsingArrayDemo {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
